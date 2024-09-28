7b4adf80daefbba40ab6f417e1e58e526a36f113
portifolio-html <evaldogusmaocosta@outlook.com>
2024-09-28 06:50:21 -0300

    19

--- js/script001.js
+++ js/script001.js
@@ -1,14 +1,11 @@
 var count = 1;
 document.getElementById("button1").checked = true;
 
-setInterval(function(){
-    NextHobbie();
-}, 2000)
-
-function NextHobbie(){
+setInterval(function NextHobbie(){
     count++;
     if(count>4){
         count = 1;
     }
     document.getElementById("button"+count).checked = true;
-}
+}, 2000)
+
